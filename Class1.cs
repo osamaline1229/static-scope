ddbe80f650dea857111ce8a7116ad9977c9b6360
Osamaline1229 <osama98138139@gmail.com>
2023-06-18 00:15:45 -0700

    scope

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class A
+{
+    int numberofWheels;
+    public static int someNumber = 2;
+
+
+    public A(int parameter)
+    {
+        numberofWheels = parameter;
+    }
+
+    public int getDoubleNumberofWheels()
+    {
+        someNumber = someNumber * 10;
+
+        return numberofWheels * 2;
+    }
+
+
+    public static void getFloatNumberofWheels()
+    {
+        Console.WriteLine("Test");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        A anyName = new A(5);
+        anyName.getDoubleNumberofWheels();
+        Console.WriteLine(A.someNumber);
+
+        A anyValue = new A(6);
+        anyValue.getDoubleNumberofWheels();
+        Console.WriteLine(A.someNumber);
+
+        anyName.getDoubleNumberofWheels();
+        Console.WriteLine(A.someNumber);
+
+    }
+}
